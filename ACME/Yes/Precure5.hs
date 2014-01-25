2d7c4d6c4c62bf29da5b8873501bb3cebb2e8481
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-01-25 15:54:12 +0900

    add transformation phrases

--- ACME/Yes/Precure5.hs
+++ ACME/Yes/Precure5.hs
@@ -11,3 +11,10 @@ instance Random Precure5 where
     let (p, g') = randomR (fromEnum a, fromEnum b) g
         in (toEnum p, g')
   random = randomR (minBound, maxBound)
+
+transformationPhrase :: Precure5 -> String
+transformationPhrase CureDream    = "大いなる希望の力、キュアドリーム！"
+transformationPhrase CureRouge    = "情熱の赤い炎、キュアルージュ！"
+transformationPhrase CureLemonade = "はじけるレモンの香り、キュアレモネード！"
+transformationPhrase CureMint     = "安らぎの緑の大地、キュアミント！"
+transformationPhrase CureAqua     = "知性の青き泉、キュアアクア！"
