c81adbbf1e0a6fe16f31255a0b1c3a0471ba2cb8
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-01-25 14:33:12 +0900

    reproduce the given argument like the GNU yes command

--- main.hs
+++ main.hs
@@ -1,5 +1,16 @@
 module Main where
 
+import Data.List
+import System.Environment
+
 main :: IO ()
 main = do
-  mapM_ putStr (repeat "y\n")
+  args <- getArgs
+  putStr $ generateInfinite args
+
+generateInfinite :: [String] -> String
+generateInfinite = unlines . repeat . generateLine
+
+generateLine :: [String] -> String
+generateLine [] = "y"
+generateLine xs = intercalate " " xs
