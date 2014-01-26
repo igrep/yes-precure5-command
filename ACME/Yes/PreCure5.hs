cfb373f93bc3ea403425668eb84acc744a8c19ac
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-01-26 22:12:13 +0900

    rename module name and type name: Precure -> PreCure
    according to http://en.wikipedia.org/wiki/Pretty_Cure

--- ACME/Yes/PreCure5.hs
+++ ACME/Yes/PreCure5.hs
@@ -1,28 +1,28 @@
-module ACME.Yes.Precure5 where
+module ACME.Yes.PreCure5 where
 
 import System.Random
 import Text.Parsec
 import Text.Parsec.String
 import Control.Applicative ( (<$>), (<*>) )
 
-data Precure5 =
+data PreCure5 =
   CureDream | CureRouge | CureLemonade | CureMint | CureAqua
   deriving (Show, Bounded, Enum)
 
-instance Random Precure5 where
+instance Random PreCure5 where
   randomR (a, b) g =
     let (p, g') = randomR (fromEnum a, fromEnum b) g
         in (toEnum p, g')
   random = randomR (minBound, maxBound)
 
-transformationPhrase :: Precure5 -> String
+transformationPhrase :: PreCure5 -> String
 transformationPhrase CureDream    = "大いなる希望の力、キュアドリーム！"
 transformationPhrase CureRouge    = "情熱の赤い炎、キュアルージュ！"
 transformationPhrase CureLemonade = "はじけるレモンの香り、キュアレモネード！"
 transformationPhrase CureMint     = "安らぎの緑の大地、キュアミント！"
 transformationPhrase CureAqua     = "知性の青き泉、キュアアクア！"
 
-fullTransformationPhrase  :: [Precure5] -> String
+fullTransformationPhrase  :: [PreCure5] -> String
 fullTransformationPhrase ps =
      "プリキュア！メタモルフォーゼ！\n"
   ++ (unlines $ map transformationPhrase ps)
@@ -30,8 +30,8 @@ fullTransformationPhrase ps =
   ++ "華麗に羽ばたく5つの心！"
   ++ "Yes！プリキュア5！"
 
-isPrecure5 :: String -> Bool
-isPrecure5 = isRight . parse precure5 "The argument"
+isPreCure5 :: String -> Bool
+isPreCure5 = isRight . parse precure5 "The argument"
 
 precure5 :: Parser String
 precure5 = (++) <$> (string "プリキュア" <|> string "PreCure") <*> (string "5" <|> string "５")
