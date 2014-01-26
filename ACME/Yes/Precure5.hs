9d81ce5e0c5bd15deff47cb47c708c62a607e6b5
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-01-26 22:00:43 +0900

    NOT TESTED: add a predicate to judge it's 'PreCure'

--- ACME/Yes/Precure5.hs
+++ ACME/Yes/Precure5.hs
@@ -1,6 +1,9 @@
 module ACME.Yes.Precure5 where
 
 import System.Random
+import Text.Parsec
+import Text.Parsec.String
+import Control.Applicative ( (<$>), (<*>) )
 
 data Precure5 =
   CureDream | CureRouge | CureLemonade | CureMint | CureAqua
@@ -26,3 +29,13 @@ fullTransformationPhrase ps =
   ++ "希望の力と未来の光！\n"
   ++ "華麗に羽ばたく5つの心！"
   ++ "Yes！プリキュア5！"
+
+isPrecure5 :: String -> Bool
+isPrecure5 = isRight . parse precure5 "The argument"
+
+precure5 :: Parser String
+precure5 = (++) <$> (string "プリキュア" <|> string "PreCure") <*> (string "5" <|> string "５")
+
+isRight :: Either a b -> Bool
+isRight (Right _) = True
+isRight (Left  _) = False
