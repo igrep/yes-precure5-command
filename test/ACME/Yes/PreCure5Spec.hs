3b6d95bf75743c6e1a0788181ce12de33f2f836e
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-01-26 22:22:59 +0900

    test other important 'being PreCure5'

--- test/ACME/Yes/PreCure5Spec.hs
+++ test/ACME/Yes/PreCure5Spec.hs
@@ -2,9 +2,16 @@ module ACME.Yes.PreCure5Spec where
 
 import ACME.Yes.PreCure5
 import Test.Hspec
+import Control.Monad
 
 spec :: Spec
 spec = do
   describe "isPreCure5" $ do
-    it "is not blank" $
+    it "is not to be blank" $
       "" `shouldSatisfy` (not . isPreCure5)
+
+    forM_ ["プリキュア5", "プリキュア５", "PreCure 5"] itIsPreCure5
+
+itIsPreCure5 :: String -> Spec
+itIsPreCure5 s =
+  it ("is to be " ++ s) $ s `shouldSatisfy` isPreCure5
