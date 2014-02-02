e1eed3a8354e0e381034c5962ce475e412d2bfea
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-02-02 11:54:06 +0900

    rename transformation functions according to the expressions in en.wikipedia.org

--- ACME/Yes/PreCure5.hs
+++ ACME/Yes/PreCure5.hs
@@ -15,17 +15,17 @@ instance Random PreCure5 where
         in (toEnum p, g')
   random = randomR (minBound, maxBound)
 
-transformationPhrase :: PreCure5 -> String
-transformationPhrase CureDream    = "大いなる希望の力、キュアドリーム！"
-transformationPhrase CureRouge    = "情熱の赤い炎、キュアルージュ！"
-transformationPhrase CureLemonade = "はじけるレモンの香り、キュアレモネード！"
-transformationPhrase CureMint     = "安らぎの緑の大地、キュアミント！"
-transformationPhrase CureAqua     = "知性の青き泉、キュアアクア！"
+introducesHerselfAs :: PreCure5 -> String
+introducesHerselfAs CureDream    = "大いなる希望の力、キュアドリーム！"
+introducesHerselfAs CureRouge    = "情熱の赤い炎、キュアルージュ！"
+introducesHerselfAs CureLemonade = "はじけるレモンの香り、キュアレモネード！"
+introducesHerselfAs CureMint     = "安らぎの緑の大地、キュアミント！"
+introducesHerselfAs CureAqua     = "知性の青き泉、キュアアクア！"
 
-fullTransformationPhrase  :: [PreCure5] -> String
-fullTransformationPhrase ps =
+transformationPhraseOf :: [PreCure5] -> String
+transformationPhraseOf ps =
      "プリキュア！メタモルフォーゼ！\n"
-  ++ (unlines $ map transformationPhrase ps)
+  ++ (unlines $ map introducesHerselfAs ps)
   ++ "希望の力と未来の光！\n"
   ++ "華麗に羽ばたく5つの心！"
   ++ "Yes！プリキュア5！"
