5c46af7c8c7c77806370e811679d1d280422e2f6
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-02-15 19:06:10 +0900

    try to make abstract functions to choose a precure
    but currently can't compile due to ambigious type

--- ACME/Yes/PreCure5/Random.hs
+++ ACME/Yes/PreCure5/Random.hs
@@ -1,37 +1,37 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module ACME.Yes.PreCure5.Random
-  ( chooseTransformationPhrase
+  ( choosePreCure
+  , chooseTransformationPhrase
   , chooseMetamorphose
   ) where
 
-import ACME.Yes.PreCure5.Profiles
+import ACME.Yes.PreCure5.Class
 import System.Random
 import Control.Monad.State
 import qualified Data.Set as S
 
-instance Random PreCure5 where
-  randomR (a, b) g =
-    let (p, g') = randomR (fromEnum a, fromEnum b) g
-        in (toEnum p, g')
-  random = randomR (minBound, maxBound)
+choosePrecure :: (RandomGen g, PreCure p) => g -> (p, g)
+choosePrecure g = (ps !! i, g')
+  where
+    pset = allPrecures
+    ps = S.toAscList pset
+    (i, g') = randomR (0, l) g
+    l = S.size pset - 1
 
 chooseTransformationPhrase :: RandomGen g => g -> (String, g)
 chooseTransformationPhrase g =
   (flip runState) g $ do
     i <- randomRSt (1, 5)
-    p <- randomSt
+    p <- state choosePrecure
     return $ transformationPhraseOf $ fiveOrSinglePreCure i p
 
 chooseMetamorphose :: RandomGen g => g -> (String, g)
 chooseMetamorphose = chooseTransformationPhrase
 
-fiveOrSinglePreCure :: Int -> PreCure5 -> S.Set PreCure5
+fiveOrSinglePreCure :: (PreCure p) => Int -> p -> S.Set p
 fiveOrSinglePreCure 5 _ = allPrecures
 fiveOrSinglePreCure _ p = S.singleton p
 
 randomRSt :: (RandomGen g, Random a) => (a, a) -> State g a
 randomRSt r = state $ randomR r
-
-randomSt :: (RandomGen g, Random a) => State g a
-randomSt = state random
