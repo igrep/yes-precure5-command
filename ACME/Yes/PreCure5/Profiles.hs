e41a3bef89a652bdaf57c5a048c136012d35799b
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-02-15 15:06:05 +0900

    add PreCure5 as an instance of PreCure

--- ACME/Yes/PreCure5/Profiles.hs
+++ ACME/Yes/PreCure5/Profiles.hs
@@ -6,28 +6,27 @@ module ACME.Yes.PreCure5.Profiles
   , metamorphoseOf
   ) where
 
+import ACME.Yes.PreCure5.Class
+
 data PreCure5 =
   CureDream | CureRouge | CureLemonade | CureMint | CureAqua
   deriving (Show, Bounded, Enum)
 
-allPrecures :: [PreCure5]
-allPrecures = [minBound..maxBound]
+instance PreCure PreCure5 where
+  allPrecures = [minBound..maxBound]
 
-introducesHerselfAs :: PreCure5 -> String
-introducesHerselfAs CureDream    = "大いなる希望の力、キュアドリーム！"
-introducesHerselfAs CureRouge    = "情熱の赤い炎、キュアルージュ！"
-introducesHerselfAs CureLemonade = "はじけるレモンの香り、キュアレモネード！"
-introducesHerselfAs CureMint     = "安らぎの緑の大地、キュアミント！"
-introducesHerselfAs CureAqua     = "知性の青き泉、キュアアクア！"
+  introducesHerselfAs CureDream    = "大いなる希望の力、キュアドリーム！"
+  introducesHerselfAs CureRouge    = "情熱の赤い炎、キュアルージュ！"
+  introducesHerselfAs CureLemonade = "はじけるレモンの香り、キュアレモネード！"
+  introducesHerselfAs CureMint     = "安らぎの緑の大地、キュアミント！"
+  introducesHerselfAs CureAqua     = "知性の青き泉、キュアアクア！"
 
-transformationPhraseOf :: [PreCure5] -> String
-transformationPhraseOf ps =
-     "プリキュア！メタモルフォーゼ！\n"
-  ++ (unlines $ map introducesHerselfAs ps)
-  ++ "希望の力と未来の光！\n"
-  ++ "華麗に羽ばたく5つの心！\n"
-  ++ "Yes！プリキュア5！\n"
+  transformationPhraseOf ps =
+       "プリキュア！メタモルフォーゼ！\n"
+    ++ (unlines $ map introducesHerselfAs ps)
+    ++ "希望の力と未来の光！\n"
+    ++ "華麗に羽ばたく5つの心！\n"
+    ++ "Yes！プリキュア5！\n"
 
 metamorphoseOf :: [PreCure5] -> String
 metamorphoseOf = transformationPhraseOf
-
