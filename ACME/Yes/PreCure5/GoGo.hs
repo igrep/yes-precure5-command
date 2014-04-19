ad0f7d3f7c66fa21bcbcb2495f255b35009ae8a7
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-04-19 15:05:46 +0900

    add directory module for Yes.PreCure5.GoGo.*

---
+++ ACME/Yes/PreCure5/GoGo.hs
@@ -0,0 +1,7 @@
+module ACME.Yes.PreCure5.GoGo
+  ( module ACME.Yes.PreCure5.GoGo.Profiles
+  , isPreCure5GoGo
+  ) where
+
+import ACME.Yes.PreCure5.GoGo.Profiles
+import ACME.Yes.PreCure5.GoGo.Parser
